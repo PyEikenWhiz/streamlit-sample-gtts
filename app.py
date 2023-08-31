c7aa62ef8006060cdd8bc9375216347a20093242
PyEikenWhize <mkimura.eiken@gmail.com>
2023-08-31 14:27:31 +0000

    Updated code

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 from gtts import gTTS
 import streamlit as st
 
-
 # Text to be converted to speech
 text = st.text_input(label="Message", value="Hello, welcome to the world of text-to-speech!")
 st.write(text)
 
 # Initialize gTTS object with the text and language (en for English)
-tts = gTTS(text, lang='en')
+selected = st.radio("Language",["English", "Japanese"])
+lang_code = {"English":"en", "Japanese":"jp"}
+tts = gTTS(text, lang=lang_code[selected])
 
 if text:
   # Save the audio to a file (you can specify the filename)
