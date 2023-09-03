6c8ce413422a7eafb69f6b1a25c6e311a2248a9b
USERNAME <MAIL>
2023-09-03 08:49:23 +0000

    Updated code

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from gtts import gTTS
 import streamlit as st
 
 # Text to be converted to speech
-text = st.text_input(label="Message", value="Hello, welcome to the world of text-to-speech!")
+text = st.text_input(label="Message", value="Hello there, welcome to the world of text-to-speech!")
 st.write(text)
 
 # Initialize gTTS object with the text and language (en for English)
-selected = st.radio("Language",["English", "Japanese"])
 lang_code = {"English":"en", "Japanese":"ja"}
+selected = st.radio("Language",["English", "Japanese"])
 tts = gTTS(text, lang=lang_code[selected])
 
 if text:
